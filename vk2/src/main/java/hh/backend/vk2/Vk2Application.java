4a2c7d6f240a652d1c7c56e23b923d137aacc231
heiniemilya <129046255+heiniemilya@users.noreply.github.com>
2026-08-29 18:46:50 +0300

    change folder name demo > vk1, add v2

---
+++ vk2/src/main/java/hh/backend/vk2/Vk2Application.java
@@ -0,0 +1,13 @@
+package hh.backend.vk2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vk2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vk2Application.class, args);
+	}
+
+}
