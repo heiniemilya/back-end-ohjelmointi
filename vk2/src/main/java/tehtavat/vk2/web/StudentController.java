7d84943ef157ffc12e1c308fa3b121a3cd39bc23
heiniemilya <129046255+heiniemilya@users.noreply.github.com>
2026-08-30 16:12:38 +0300

    exercise 2

--- vk2/src/main/java/tehtavat/vk2/web/StudentController.java
+++ vk2/src/main/java/tehtavat/vk2/web/StudentController.java
@@ -14,13 +14,17 @@ import tehtavat.vk2.domain.Student;
 
 @Controller
 public class StudentController {
-    @RequestMapping("hellostudents")
-    public String listStudents(Model model) {
-        List<Student> students = new ArrayList<>();
+
+    public static final List<Student> students = new ArrayList<>();
+
+    static {
         students.add(new Student("Kate", "Cole"));
         students.add(new Student("Dan", "Brown"));
         students.add(new Student("Mike", "Mars"));
+    }
 
+    @RequestMapping("hellostudents")
+      public String listStudents(Model model) {
         model.addAttribute("students", students);
         return "hellostudents";
     }
