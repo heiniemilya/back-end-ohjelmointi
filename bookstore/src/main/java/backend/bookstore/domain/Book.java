039d34aa90b9645c4b800bff688400f29b9acfcc
heiniemilya <129046255+heiniemilya@users.noreply.github.com>
2026-09-09 16:03:52 +0300

    create entity

--- bookstore/src/main/java/backend/bookstore/domain/Book.java
+++ bookstore/src/main/java/backend/bookstore/domain/Book.java
@@ -1,6 +1,15 @@
 package backend.bookstore.domain;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
     private String title;
     private String author;
     private int publicationYear;
